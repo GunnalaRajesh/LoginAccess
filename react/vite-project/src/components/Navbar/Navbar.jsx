dfd45f43600b363aa812b475ce73c47b3b051cc9
ShaikHussamali <122262727+ShaikHussamali@users.noreply.github.com>
2023-11-02 17:07:46 +0530

    converted to react app and navbar added

---
+++ react/vite-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+
+
+function Navbar() {
+  return (
+    <div>
+  <div className="container" style={{ maxWidth: '1200px' }}>
+  <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
+  <nav className="navbar navbar-light" style={{ backgroundColor: 'lightblue', maxWidth: '1200px' }}>
+    <a className="navbar-brand" href="#">
+      <img src="./src/assets/Images/logo.png" width="130" height="130" alt="" />
+    </a>
+    <a className="navbar-brand" href="#">
+      <img src="./src/assets/Images/ali.jpg" width="130" height="130" alt="" />
+    </a>
+    <a className="navbar-brand" href="#">
+      <img src="./src/assets/Images/barre.jpg" width="100" height="130" alt="" />
+    </a>
+    <a className="navbar-brand" href="#">
+      <img src="./src/assets/Images/rajesh.jpg" width="130" height="130" alt="" />
+    </a>
+    <a className="navbar-brand" href="#">
+      <img src="./src/assets/Images/shiva.jpg" width="100" height="130" alt="" />
+    </a>
+  </nav>
+</div>
+
+  </div>
+</div>
+
+  )
+}
+
+export default Navbar
