34c4acd9d9120fb5fce9bac6b872e32d95b1b84b
ShaikHussamali <122262727+ShaikHussamali@users.noreply.github.com>
2023-11-03 18:33:40 +0530

    0Auth added
    Login and logout working properly

--- react/vite-project/src/components/Navbar/Navbar.jsx
+++ react/vite-project/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
+  const { loginWithRedirect } = useAuth0();
+  const {logout}=useAuth0();
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div>
   <div className="container" style={{ maxWidth: '1200px' }}>
@@ -9,21 +13,27 @@ function Navbar() {
     <a className="navbar-brand" href="#">
       <img src="./src/assets/Images/logo.png" width="130" height="130" alt="" />
     </a>
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="https://www.linkedin.com/in/shaik-hussamali-ba7484225/">
       <img src="./src/assets/Images/ali.jpg" width="130" height="130" alt="" />
     </a>
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="https://www.linkedin.com/in/silvoj-rajesh-kumar-ba655b1a7/#">
       <img src="./src/assets/Images/barre.jpg" width="100" height="130" alt="" />
     </a>
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="https://www.linkedin.com/in/gunnala-rajesh-556a73163/">
       <img src="./src/assets/Images/rajesh.jpg" width="130" height="130" alt="" />
     </a>
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="https://www.linkedin.com/in/shivasaikrishnagadeela/">
       <img src="./src/assets/Images/shiva.jpg" width="100" height="130" alt="" />
     </a>
+    {
+      isAuthenticated ? (
+        <button  onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} type="button" style={{ padding:"10px"}} className="btn btn-primary btn-lg">Logout</button>
+      ):(
+         <button  onClick={() => loginWithRedirect()} type="button" style={{ padding:"10px"}} className="btn btn-primary btn-lg">Login</button>
+      )
+    }
   </nav>
 </div>
-
   </div>
 </div>
 
--- react/vite-project/src/main.jsx
+++ react/vite-project/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import {Auth0Provider} from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-akm2u2gd6g277n1j.us.auth0.com"
+    clientId="SiRre9MaH30vE2ABwDcwpbbOrZMm20h6"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>,
+);
